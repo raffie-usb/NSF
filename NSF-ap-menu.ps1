d7862cf241b42d7bf94b6c0047b07a8c1662e91c
Jesse Mulder <jesse.mulder@giraffe-it.nl>
2026-02-18 11:36:21 +0100

    First commit

---
+++ NSF-ap-menu.ps1
@@ -0,0 +1,77 @@
+#================================================
+# Window Functions
+# Minimize Command and PowerShell Windows
+#================================================
+$Script:showWindowAsync = Add-Type -MemberDefinition @"
+[DllImport("user32.dll")]
+public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+"@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
+function Hide-CmdWindow() {
+    $CMDProcess = Get-Process -Name cmd -ErrorAction Ignore
+    foreach ($Item in $CMDProcess) {
+        $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $Item.id).MainWindowHandle, 2)
+    }
+}
+function Hide-PowershellWindow() {
+    $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)
+}
+function Show-PowershellWindow() {
+    $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 10)
+}
+Hide-CmdWindow
+Hide-PowershellWindow
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Computer Profile'
+$form.Size = New-Object System.Drawing.Size(500,400)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(150,300)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(225,300)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please select a profile:'
+$form.Controls.Add($label)
+
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Location = New-Object System.Drawing.Point(10,40)
+$listBox.Size = New-Object System.Drawing.Size(260,20)
+$listBox.Height = 240
+$listBox.Width  = 300
+[void] $listBox.Items.Add('--------REG--------')
+[void] $listBox.Items.Add('MobieleWerkplek')
+[void] $listBox.Items.Add('ClassicMobieleWerkplek')
+[void] $listBox.Items.Add('SharedUserDevice')
+[void] $listBox.Items.Add('')
+
+
+$form.Controls.Add($listBox)
+
+$form.Topmost = $true
+
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $listBox.SelectedItem
+    $GroupTag = $x
+}
+Show-PowershellWindow
---
+++ NSF-main.ps1
@@ -0,0 +1,121 @@
+<#
+Loads Functions
+Creates Setup Complete Files
+#>
+
+Set-ExecutionPolicy Bypass -Force
+iex (irm https://raw.githubusercontent.com/jessemuldergit/REG/refs/heads/main/REG-ap-menu.ps1)
+
+Write-Host -Foreground Red $GroupTag
+sleep -Seconds 3
+
+iex (irm https://raw.githubusercontent.com/jessemuldergit/REG/refs/heads/main/REG-functions.ps1)
+
+#++++++++++++++++++++++++++++++
+# Functions were here !!
+#++++++++++++++++++++++++++++++
+
+Set-ExecutionPolicy Bypass -Force
+
+#WinPE Stuff
+if ($env:SystemDrive -eq 'X:') {
+    #Create Custom SetupComplete on USBDrive, this will get copied and run during SetupComplete Phase thanks to OSD Function: Set-SetupCompleteOSDCloudUSB
+    #Set-SetupCompleteCreateStartHOPEonUSB
+    #Set-SetupCompleteOSDCloudUSB
+    
+    #Write-Host -ForegroundColor Green "Starting win11.garytown.com"
+    #to Run boot OSDCloudUSB, at the PS Prompt: iex (irm win11.garytown.com)
+
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$OSVersion = 'Windows 11' #Used to Determine Driver Pack
+$OSReleaseID = '25H2' #Used to Determine Driver Pack
+$OSName = 'Windows 11 25H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'nl-NL'
+
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$true
+    SyncMSUpCatDriverUSB = [bool]$true
+    ApplyCatalogFirmware = [bool]$true
+   }
+
+#Used to Determine Driver Pack
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+
+<#
+#Enable HPIA | Update HP BIOS | Update HP TPM
+if (Test-HPIASupport){
+    #$Global:MyOSDCloud.DevMode = [bool]$True
+    $Global:MyOSDCloud.HPTPMUpdate = [bool]$True
+    if ($Product -ne '83B2'){$Global:MyOSDCloud.HPIAALL = [bool]$true} #I've had issues with this device and HPIA
+    $Global:MyOSDCloud.HPBIOSUpdate = [bool]$true
+
+    #Set HP BIOS Settings to what I want:
+    iex (irm https://raw.githubusercontent.com/MSP-AVG/TF/refs/heads/main/tf-manage-hpbiossettings.ps1)
+    Manage-HPBiosSettings -SetSettings
+}
+#>
+
+#write variables to console
+Write-Output $Global:MyOSDCloud
+
+#Update Files in Module that have been updated since last PowerShell Gallery Build (Testing Only)
+$ModulePath = (Get-ChildItem -Path "$($Env:ProgramFiles)\WindowsPowerShell\Modules\osd" | Where-Object {$_.Attributes -match "Directory"} | select -Last 1).fullname
+import-module "$ModulePath\OSD.psd1" -Force
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+write-host "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot" -ForegroundColor Green
+
+
+
+#Copy CMTrace Local:
+if (Test-path -path "x:\windows\system32\cmtrace.exe"){
+    copy-item "x:\windows\system32\cmtrace.exe" -Destination "C:\Windows\System\cmtrace.exe"
+}
+
+$GroupTag | Out-File -FilePath C:\Windows\DeviceType.txt
+
+Set-SetupCompleteOSDCloudUSB
+
+#Save Windows Image on USB 
+$OSDCloudUSB = Get-Volume.usb | Where-Object {($_.FileSystemLabel -match 'OSDCloud') -or ($_.FileSystemLabel -match 'BHIMAGE')} | Select-Object -First 1
+$DriverPath = "$($OSDCloudUSB.DriveLetter):\OSDCloud\OS\"
+$ImageFileName = Get-ChildItem -Path $DriverPath -Name *.esd
+$ImageFileNameDL = Get-ChildItem -Path 'C:\OSDCloud\OS' -Name *.esd 
+if (!(Test-Path $DriverPath)){New-Item -ItemType Directory -Path $DriverPath}
+
+if($ImageFileName -ne $ImageFileNameDL){
+    Remove-Item -Path $DriverPath$ImageFileName -Force
+if (!(Test-Path $DriverPath)){New-Item -ItemType Directory -Path $DriverPath}
+if (!(Test-Path $DriverPath$ImageFileNameDL)){Copy-Item -Path C:\OSDCloud\OS\$ImageFileNameDL -Destination $DriverPath$ImageFileNameDL -Force}
+}
+#===================
+
+if($GroupTag -eq 'MobieleWerkplek'){Dism /image:C:\ /Set-InputLocale:0413:00020409}
+if($GroupTag -eq 'ClassicMobieleWerkplek'){Dism /image:C:\ /Set-InputLocale:0413:00020409}
+if($GroupTag -eq 'SharedUserDevice'){Dism /image:C:\ /Set-InputLocale:0413:00020409}
+
+Restart-Computer
+}
